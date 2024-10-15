76f18a71822d097d6a8091ba4bbafa3c1aba095a
Aw1nger <aw1nger@yandex.ru>
2024-10-15 15:05:03 +0300

    make the user profile

---
+++ src/app/(private)/profile/page.tsx
@@ -0,0 +1,11 @@
+import { UserProfile } from "@/features/user-profile";
+
+const Page = () => {
+  return (
+    <section className="container py-10">
+      <UserProfile />
+    </section>
+  );
+};
+
+export default Page;
